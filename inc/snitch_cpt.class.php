b58ad3ba8b1c7b4a5b07e4bc2ebe764f22c6f204
timse201 <timse201@users.noreply.github.com>
2017-01-25 17:57:56 +0100

    support added

--- inc/snitch_cpt.class.php
+++ inc/snitch_cpt.class.php
@@ -994,8 +994,9 @@ class Snitch_CPT
 	public static function views_edit($views)
 	{
 		$links = array(
-			'paypal' => '<a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=8CH5FPR88QYML" target="_blank" rel="noopener noreferrer">Donate</a>',
-			'wiki' => '<a href="https://github.com/pluginkollektiv/snitch/wiki" target="_blank" rel="noopener noreferrer">Manual</a>';
+			'paypal' => '<a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=8CH5FPR88QYML" target="_blank" rel="noopener noreferrer">' . esc_html__( 'Donate', 'snitch' ) . '</a>',
+			'wiki' => '<a href="https://github.com/pluginkollektiv/snitch/wiki" target="_blank" rel="noopener noreferrer">' . esc_html__( 'Manual', 'snitch' ) . '</a>'
+			'support' => '<a href="https://wordpress.org/support/plugin/snitch" target="_blank" rel="noopener noreferrer">' . esc_html__( 'Support', 'snitch' ) . '</a>'
 		);
 
 		return $links;
